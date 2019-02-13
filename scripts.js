8513ed4d21a57a3c1f31b59ec9001091a9868df9
wesleyap <wesley.apstos@outlook.com>
2019-02-13 21:22:41 -0200

    Continuar estrutura do jogo

--- scripts.js
+++ scripts.js
@@ -1,21 +1,39 @@
-const aleatorio = function (min = 0, max =0){
-    return Math.floor(math.random()*(max-min+1))+min;
+const aleatorio = function (min = 0, max = 50) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
-console.log('oi')
-let sorteado = aleatorio(0, 50) 
+
+
+let sorteado = aleatorio(0, 50)
+
 
 let resposta = Number(prompt('Adivinhe qual é o número. Lembre-se que ele está entre 0 e 50 ;)'));
 
-i=10;
-while(i>){
-    if(resposta === sorteado){ 
-        alert('Acertô miseravi!!!!')
+let i = 0;
+let vidas = 9;
+
+while ((vidas < i) || (resposta < sorteado) || (resposta > sorteado) || (resposta == sorteado)) {
+    if (resposta == sorteado) {
+        alert('Acertô miseravi!!!!');
     }
-    else if(resposta > sorteado){
+
+
+    else if (resposta > sorteado) {
+        resposta = (prompt('Errou, chute mais baixo'));
+        
 
     }
-    else{
 
+    else if (resposta < sorteado) {
+        resposta = (prompt('Errou, chute mais alto'));
     }
-    i--;
+
+     vidas-1;
+
+
+}
+
+
+if(vidas< i){
+    alert("Você perdeu tente na proxima")
 }
