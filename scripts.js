ca5506e994de4d9c7f01d3c0a42ef4c09a4d5225
Ana Clara <acfm22.2001@gmail.com>
2019-02-13 19:17:04 -0200

    Exercício de adivinhação

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const aleatorio = function (min = 0, max =0){
-    return Math.floor(math.random()*(max-min+1))+min;
+    return Math.floor(Math.random()*(max-min+1))+min;
 }
 console.log('oi')
 let sorteado = aleatorio(0, 50) 
@@ -7,15 +7,19 @@ let sorteado = aleatorio(0, 50)
 let resposta = Number(prompt('Adivinhe qual é o número. Lembre-se que ele está entre 0 e 50 ;)'));
 
 i=10;
-while(i>){
+while(i>0){
     if(resposta === sorteado){ 
-        alert('Acertô miseravi!!!!')
+        alert('Acertô miseravi!!!!');
     }
     else if(resposta > sorteado){
-
+        let resposta= Number(prompt('Não foi dessa vez...seu número é um pouco menor.'));
+    }
+    else if(resposta<sorteado){
+        let resposta=Number(prompt('Não foi dessa vez...seu número é um pouco maior.'));
     }
     else{
-
+        alert('Você perdeu. Boa sorte na próxima. Otário hahahah');
     }
+    
     i--;
 }
